ec13ba8df43adb9d66fddee757e0dfce7e203000
Jake <jdklassen1@gmail.com>
2022-02-19 01:19:01 -0800

    Final Friday

--- dnstopsites.sh
+++ dnstopsites.sh
@@ -27,24 +27,13 @@ normal=$(tput sgr0)
 NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
 
 # Domains to test. Duplicates are fine
-DOMAINS2TEST=$(<./topsites.txt)
+domains=$(<./topsites.txt)
+domainnum=`echo "$domains" | wc -l`
 
 clear
-
 printf "${bold}DNS Speed Testing Top 500 Sites${normal}\n"
 printf "DNS: ${NAMESERVERS%%#*}\n"
 printf "DNS Name: ${NAMESERVERS##*#}\n\n"
-
-# Setting it to zero for the counter
-totaldomains=0
-
-totaldomains=0
-
-for d in $DOMAINS2TEST; do
-    # Simple counter
-    totaldomains=$((totaldomains + 1))
-
-done
 
 # Just parsing it and removing post pound symbol
 # Also setting some variables to start the script
@@ -54,7 +43,7 @@ for i in $(seq $count); do
     ftime=0
     # Print Result Each time
     # printf "%-18s" "$pname"
-    for d in $DOMAINS2TEST; do
+    for d in $domains; do
         ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
         if [ -z "$ttime" ]; then
 	        #let's have time out be 1s = 1000ms
@@ -71,20 +60,20 @@ for i in $(seq $count); do
         then
         printf "%-8s" "${red}ERR${normal}"
         printf "%-5s"
-        #printf "%-8s" "ERR"
         else
           printf "%-8s" "$ttime ms"
         fi
 
         #Add each time
         ftime=$((ftime + ttime))
-        # ftime=$((ftime + ttime))
     done
     # Average Calc
-    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+    avg=`bc -lq <<< "scale=2; $ftime/$domainnum"`
     # Average Print
-    printf "\n"
-    printf "Average Time for DNS Request:  $avg"
+    printf "\n\n"
+    printf ${domainnum}
+    printf " domains averaging ${avg}ms\n\n"
+
 
 done
 
